a2ae558da0a4206c1a564fbf3610eabb65f329f7
vinsburg <vinsburg@gmail.com>
2023-06-03 14:55:30 +0300

    Pass remove duplicate test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+use std::collections::HashSet;
+
 pub fn remove_duplicate(list: &Vec<i32>) -> Vec<i32>{
-    list.clone()
+    let mut set: HashSet<i32> = HashSet::new();
+    let mut new_list: Vec<i32> = Vec::new();
+
+    for &item in list {
+        if !set.contains(&item) {
+            set.insert(item);
+            new_list.push(item);
+        }
+    }
+
+    new_list
 }
