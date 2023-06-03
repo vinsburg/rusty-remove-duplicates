3cc26905fe7b004feaa206638aad021699e6a103
vinsburg <vinsburg@gmail.com>
2023-06-03 14:59:38 +0300

    test that original list is changed

--- tests/lib_tests.rs
+++ tests/lib_tests.rs
@@ -5,24 +5,26 @@ use remove_duplicate;
 
 #[test]
 fn test_empty_list() {
-    let list: Vec<i32> = Vec::new();
-    let read_list: Vec<i32> = remove_duplicate::remove_duplicate(&list);
-    assert_eq!(read_list, list);
+    let mut list: Vec<i32> = Vec::new();
+    let expected_list: Vec<i32> = Vec::new();
+    remove_duplicate::remove_duplicate(&list);
+    assert_eq!(list, expected_list);
 }
 
 
 #[test]
 fn test_two_member_list() {
-    let list: Vec<i32> = vec![1, 2];
-    let read_list: Vec<i32> = remove_duplicate::remove_duplicate(&list);
-    assert_eq!(read_list, list);
+    let mut list: Vec<i32> = vec![1, 2];
+    let expected_list: Vec<i32> = vec![1, 2];
+    remove_duplicate::remove_duplicate(&list);
+    assert_eq!(list, expected_list);
 }
 
 
 #[test]
 fn test_multiple_duplicates() {
-    let list: Vec<i32> = vec![1, 1, 2, 2, 3, 3];
+    let mut list: Vec<i32> = vec![1, 1, 2, 2, 3, 3];
     let expected_list: Vec<i32> = vec![1, 2, 3];
-    let read_list: Vec<i32> = remove_duplicate::remove_duplicate(&list);
-    assert_eq!(read_list, expected_list);
+    remove_duplicate::remove_duplicate(&list);
+    assert_eq!(list, expected_list);
 }
