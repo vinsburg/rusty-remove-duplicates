ef780f75069079abec0f4906b7330f8cd0f9ae62
vinsburg <vinsburg@gmail.com>
2023-06-03 14:41:13 +0300

    Add test for multiple duplicates

--- tests/lib_tests.rs
+++ tests/lib_tests.rs
@@ -17,3 +17,12 @@ fn test_two_member_list() {
     let read_list: Vec<i32> = remove_duplicate::remove_duplicate(&list);
     assert_eq!(read_list, list);
 }
+
+
+#[test]
+fn test_multiple_duplicates() {
+    let list: Vec<i32> = vec![1, 1, 2, 2, 3, 3];
+    let expected_list: Vec<i32> = vec![1, 2, 3];
+    let read_list: Vec<i32> = remove_duplicate::remove_duplicate(&list);
+    assert_eq!(read_list, expected_list);
+}
